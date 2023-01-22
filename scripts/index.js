201334f66140153fd57a4a2c9a50009bfab920cf
sanjeev sharma <sanjeev.sharmab124@gmail.com>
2023-01-23 00:31:46 +0530

    sign-in

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,16 @@
-document.getElementById("login-btn").addEventListener
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+let btn =document.getElementById("login-btn")
+btn.addEventListener("click",()=>{
+    let obj ={
+        username:username.value,
+        password:password.value
+    }
+    console.log(obj);
+    if(obj.username==="admin" && obj.password==="admin"){
+      alert("Successfully Loged In");
+        location.replace("home.html")
+    }else{
+       alert("Wrong Credidentials");
+    }
+})
